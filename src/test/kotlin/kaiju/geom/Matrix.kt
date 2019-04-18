3d2b7705cdf5ee2bf4770896ad98ac2cec8f915b
stewsters <stewsters@gmail.com>
2019-04-17 22:41:41 -0500

    Printing

---
+++ src/test/kotlin/kaiju/geom/Matrix.kt
@@ -0,0 +1,24 @@
+package kaiju.geom
+
+import kaiju.math.Matrix2d
+import org.junit.Test
+
+
+class Matrix {
+
+    @Test
+    fun testPrint() {
+
+        val matrix = Matrix2d(3, 3, { x, y -> x * y })
+
+        matrix.forEachIndexed { x, y, value ->
+            if (x == matrix.getSize().x - 1)
+                println("$value ")
+            else
+                print("$value ")
+        }
+
+    }
+
+
+}
