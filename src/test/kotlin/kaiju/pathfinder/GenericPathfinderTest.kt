7c4133464356bd0d3ebbd68262c19acbe2df786d
Adrian <stewsters@gmail.com>
2023-07-31 16:50:33 -0500

    updating kotlin version, looks like there may be an issue with heuristics that give an Int rather than a double in the generic pathfinder.  I may need to make it take either.

--- src/test/kotlin/kaiju/pathfinder/GenericPathfinderTest.kt
+++ src/test/kotlin/kaiju/pathfinder/GenericPathfinderTest.kt
@@ -6,7 +6,9 @@ import kaiju.math.Vec2
 import kaiju.math.Vec3
 import kaiju.math.geom.InverseRectangle
 import kaiju.math.geom.Rectangle
+import kaiju.math.getChebyshevDistance
 import kaiju.math.getEuclideanDistance
+import kaiju.math.getManhattanDistance
 import kaiju.math.matrix2dOf
 import org.junit.Test
 import kotlin.math.min
@@ -101,8 +103,8 @@ class GenericPathfinderTest {
         val end = Vec2(19, 29)
 
         val path = findGenericPath(
-            { old, pos ->
-                val d: Double = obstacles.map { it.minDist(pos) }.minOrNull()!!
+            cost = { old, pos ->
+                val d: Double = obstacles.minOf { it.minDist(pos) }
                 when {
                     d <= 0 -> {
                         Double.MAX_VALUE
@@ -118,10 +120,10 @@ class GenericPathfinderTest {
                     }
                 }
             },
-            { one, two -> 1.0 },
-            { it.mooreNeighborhood().filter { bound.contains(it.x, it.y) } },//Vec2::mooreNeighborhood,
-            start,
-            end
+            heuristic = {one,two-> getChebyshevDistance(one,two).toDouble()},
+            neighbors = { it.mooreNeighborhood().filter { bound.contains(it.x, it.y) } },//Vec2::mooreNeighborhood,
+            start = start,
+            end = end
         )
 
 
@@ -143,11 +145,11 @@ class GenericPathfinderTest {
         val end = Vec3(19, 29, 39)
 
         val path = findGenericPath(
-            { old, pos -> 1.0 },
-            { one, two -> getEuclideanDistance(one, two) },
-            Vec3::vonNeumanNeighborhood,
-            start,
-            end
+            cost = { old, pos -> 1.0 },
+            heuristic = ::getEuclideanDistance,
+            neighbors = Vec3::vonNeumanNeighborhood,
+            start = start,
+            end = end
         )
 
         when (path) {
